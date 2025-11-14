17853c86fbff1dde4cb0085ce80f0872228451cc
cibi619 <cibi.mit@gmail.com>
2025-11-14 01:04:27 -0500

    sql file changes..

--- flyway/migrations_incremental/V2__add_status_and_index.sql
+++ flyway/migrations_incremental/V2__add_status_and_index.sql
@@ -1,3 +1,4 @@
 ALTER TABLE subscribers
     ADD COLUMN status VARCHAR(20) NOT NULL DEFAULT 'active';
-CREATE INDEX idx_created_at ON subscribers(created_at);
+CREATE INDEX idx_status ON subscribers(status);
+
